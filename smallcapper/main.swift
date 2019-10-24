92754a151a831478fe9035b29beab01a0e09617f
Ryan Forsythe <ryan@pdxcb.net>
2019-10-24 11:57:55 -0700

    Tʀᴀɴꜱꜰᴏʀᴍꜱ ᴛᴇxᴛ.

--- smallcapper/main.swift
+++ smallcapper/main.swift
@@ -1,12 +1,33 @@
-//
-//  main.swift
-//  smallcapper
-//
-//  Created by ryan on 10/24/19.
-//  Copyright © 2019 ryan. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+let scMap: [Character: Character] = [
+    "a": "ᴀ",
+    "b": "ʙ",
+    "c": "ᴄ",
+    "d": "ᴅ",
+    "e": "ᴇ",
+    "f": "ꜰ",
+    "g": "ɢ",
+    "h": "ʜ",
+    "i": "ɪ",
+    "j": "ᴊ",
+    "k": "ᴋ",
+    "l": "ʟ",
+    "m": "ᴍ",
+    "n": "ɴ",
+    "o": "ᴏ",
+    "p": "ᴘ",
+    "q": "ǫ",
+    "r": "ʀ",
+    "s": "ꜱ",
+    "t": "ᴛ",
+    "u": "ᴜ",
+    "v": "ᴠ",
+    "w": "ᴡ",
+    "x": "x",
+    "y": "ʏ",
+    "z": "ᴢ"
+]
 
+let input = CommandLine.arguments.suffix(from: 1).joined(separator: " ")
+print(String(input.map({ c in scMap[c] ?? c })))
